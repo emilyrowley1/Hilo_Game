3f0bcea345f013475a4bc044c2e3170d65f84061
Emily Rowley <erowley49@gmail.com>
2022-05-16 11:16:44 -0600

    Cards and Player classes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! my name is Emily");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player();
+            Card newCard = new Card();
+            Console.WriteLine(newCard.drawCard());
+        }
+    }
+}
